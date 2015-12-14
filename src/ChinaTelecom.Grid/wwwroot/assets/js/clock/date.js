2e1e27a077db164c10eb73348c7ce7b023056673
あまみや ゆうこ <1@1234.sh>
2015-12-14 10:29:30 +0800

    Translate month and day

--- src/ChinaTelecom.Grid/wwwroot/assets/js/clock/date.js
+++ src/ChinaTelecom.Grid/wwwroot/assets/js/clock/date.js
@@ -1,7 +1,7 @@
-var monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
+﻿var monthNames = ["一月", "二月", "三月", "四月", "五月", "六月",
+    "七月", "八月", "九月", "十月", "十一月", "十二月"
 ];
-var dayNames = ["Sun, ", "Mon, ", "Tue, ", "Wed, ", "Thu, ", "Fri, ", "Sat, "]
+var dayNames = ["星期日, ", "星期一, ", "星期二, ", "星期三, ", "星期四, ", "星期五, ", "星期六, "]
 
 var newDate = new Date();
 newDate.setDate(newDate.getDate() + 1);
