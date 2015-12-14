f7cc5aa1fec7fb59d0d2de05d88b5c424b572752
あまみや ゆうこ <1@1234.sh>
2015-12-14 23:09:01 +0800

    Refectory weather bar

--- src/ChinaTelecom.Grid/Controllers/BaseController.cs
+++ src/ChinaTelecom.Grid/Controllers/BaseController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNet.Mvc;
 using ChinaTelecom.Grid.Models;
 
 namespace ChinaTelecom.Grid.Controllers
 {
     public class BaseController : BaseController<GridContext, User, string>
     {
+        public override void Prepare()
+        {
+            base.Prepare();
+            ViewBag.HeaderRecordCount = DB.Records.Count();
+            ViewBag.HeaderUserCount = DB.Users.Count();
+            ViewBag.HeaderEstateCount = DB.Estates.Count();
+        }
     }
 }
